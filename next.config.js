4214dbf705b5d29e486946d114e4300623852a3b
iwakbhetok <abdul.createit@gmail.com>
2026-02-20 20:01:26 +0700

    update next config

--- next.config.js
+++ next.config.js
@@ -1,11 +1,21 @@
 const { i18n } = require('./next-i18next.config');
 
+/** @type {import('next').NextConfig} */
 module.exports = {
   i18n,
+
   env: {
     BASE_URL: process.env.BASE_URL || 'http://localhost:3000',
   },
+
   images: {
-    domains: ['ops.housejapanesecurry.com'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'ops.housejapanesecurry.com',
+        port: '',
+        pathname: '/storage/**',
+      },
+    ],
   },
 };
